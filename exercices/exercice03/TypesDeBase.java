7d969b25452fa83d0082199d34ed25366624ff6e
shala-leon-emf <leon.shala@studentfr.ch>
2024-09-05 09:09:46 +0200

    mon exercice sur les types de bases

---
+++ exercices/exercice03/TypesDeBase.java
@@ -0,0 +1,11 @@
+public class TypesDeBase{
+    public static void main(String[] args) {
+        int age = 16;
+        String nom = "Leon";
+        System.out.println("Je m'appelle "+ nom + "et j'ai "+age+ "ans.");
+    
+
+    }
+
+
+}
