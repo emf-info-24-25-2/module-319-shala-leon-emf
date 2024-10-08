3b314708fe97327238d3d29b1754c14c772f13be
shala-leon-emf <leon.shala@studentfr.ch>
2024-10-08 19:05:32 +0200

    mon devoir 5

---
+++ devoirs/devoir03/eee.java
@@ -0,0 +1,16 @@
+public class eee {
+    public static void main(String[] args) {
+
+        public class BoucleDoWhile {
+            public static void main(String[] args) {
+                int i = 1; // Initialisation de la variable de contrôle
+        
+                do {
+                    System.out.println("Nombre : " + i);
+                    i++; // Mise à jour de la variable de contrôle
+                } while (i <= 5); // Condition de la boucle
+            }
+        }
+        
+    }
+}
---
+++ devoirs/devoir05/devoir05.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class devoir05 {
+public static void main(String[] args) {
+        // Initialiser le scanner pour lire les entrées de l'utilisateur
+        Scanner scanner = new Scanner(System.in);
+        
+        // Générer un nombre aléatoire entre 1 et 100
+        Random random = new Random();
+        int nombreMystere = random.nextInt(100) + 1;
+
+        // Initialiser la variable qui stockera la tentative de l'utilisateur
+        int tentative = 0;
+
+        // Boucle pour répéter les essais jusqu'à ce que le nombre soit trouvé
+        while (tentative != nombreMystere) {
+            // Demander à l'utilisateur de deviner un nombre
+            System.out.print("Devinez un nombre entre 1 et 100 : ");
+            tentative = scanner.nextInt();
+
+            // Vérifier si le nombre est trop petit, trop grand ou correct
+            if (tentative < nombreMystere) {
+                System.out.println("Trop petit !");
+            } else if (tentative > nombreMystere) {
+                System.out.println("Trop grand !");
+            } else {
+                System.out.println("Bravo, trouvé !");
+            }
+        }
+
+        // Fermer le scanner (bonne pratique)
+        scanner.close();
+    }
+    
+}
